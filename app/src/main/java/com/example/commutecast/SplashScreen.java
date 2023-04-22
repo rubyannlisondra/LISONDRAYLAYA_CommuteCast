611c9b3b933b92def03ad6c7a5dc21f721dc8aca
Ruby Ann <rubyannlisondra2@gmail.com>
2023-04-22 23:55:31 +0800

    SplashScreen-AccessLocation

--- app/src/main/java/com/example/commutecast/SplashScreen.java
+++ app/src/main/java/com/example/commutecast/SplashScreen.java
@@ -28,7 +28,7 @@ public class SplashScreen extends AppCompatActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                 } finally {
-                    Intent intent = new Intent(SplashScreen.this, Login.class);
+                    Intent intent = new Intent(SplashScreen.this, AccessLocation.class);
                 }
 
             }
@@ -37,7 +37,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this,Login.class);
+                Intent intent = new Intent(SplashScreen.this,AccessLocation.class);
                 startActivity(intent);
                 finish();
             }
