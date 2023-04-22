ac6deb945af70bf2a81199648783ca36e1291c25
Ruby Ann <rubyannlisondra2@gmail.com>
2023-04-22 23:48:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/commutecast/AccessLocation.java
@@ -0,0 +1,18 @@
+package com.example.commutecast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccessLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_access_location);
+
+        getSupportActionBar().hide();
+
+
+    }
+}
---
+++ app/src/main/java/com/example/commutecast/SplashScreen.java
@@ -0,0 +1,46 @@
+package com.example.commutecast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, Login.class);
+                }
+
+            }
+        }; thread.start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
